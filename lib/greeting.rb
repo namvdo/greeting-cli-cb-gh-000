f782d6a79df9e6e6337f1ab8fdfff6d25a53d769
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-01-28 13:58:46 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,6 @@
 # code the #greeting method here!
 def greeting(name)
-"Hello #{gets.name}.It is nice to meet you"
+"Hello #{gets.strip.name}.It is nice to meet you"
 puts "Hi! I'm HAL, what's your name?"
 name = gets.chomp
 gets.strip
